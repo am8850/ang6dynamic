1b69977f361a61ec988cbabb927dd08203bdea14
Alejandro Morales <Alex@alejandros-mbp.attlocal.net>
2018-10-04 16:32:11 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { MatDialog } from '@angular/material';
+
+import { DynamicComponentLoader } from './dynamic-component-loader/dynamic-component-loader.service';
+import { DialogComponent } from './dynamic-modules/dialog/dialog.component';
+import { MessageComponent } from './dynamic-modules/message/message.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DynamicLoad';
+
+  @ViewChild('testOutlet', { read: ViewContainerRef }) testOutlet: ViewContainerRef;
+
+  constructor(
+    private dynamicComponentLoader: DynamicComponentLoader,
+    private dialog: MatDialog,
+  ) {
+  }
+
+  loadComponent() {
+    this.dynamicComponentLoader
+      .getComponentFactory<MessageComponent>('message')
+      .subscribe(componentFactory => {
+        const ref = this.testOutlet.createComponent(componentFactory);
+        ref.changeDetectorRef.detectChanges();
+      }, error => {
+        console.warn(error);
+      });
+  }
+
+  showDialog() {
+    this.dialog.open(DialogComponent);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatDialogModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  DynamicComponentLoaderModule,
+  DynamicComponentManifest,
+} from './dynamic-component-loader/dynamic-component-loader.module';
+import { DialogComponent } from './dynamic-modules/dialog/dialog.component';
+import { DialogModule } from './dynamic-modules/dialog/dialog.module';
+
+// This array defines which "componentId" maps to which lazy-loaded module.
+const manifests: DynamicComponentManifest[] = [
+  {
+    componentId: 'message',
+    path: 'dynamic-message', // some globally-unique identifier, used internally by the router
+    loadChildren: './dynamic-modules/message/message.module#MessageModule',
+  },
+  {
+    componentId: 'dialog',
+    path: 'dialog',
+    loadChildren: './dynamic-modules/dialog/dialog.module#DialogModule',
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    DialogModule,
+
+    MatDialogModule,
+    NoopAnimationsModule,
+
+    DynamicComponentLoaderModule.forRoot(manifests),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+  ],
+  entryComponents: [
+    DialogComponent,
+  ],
 })
-export class AppModule { }
+export class AppModule {
+}
